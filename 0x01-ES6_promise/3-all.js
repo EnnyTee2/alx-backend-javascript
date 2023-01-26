b3fcf499f1aaeceb2c259d64410fcf70b53bcbfa
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-26 04:05:52 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,9 +5,8 @@ export default function handleProfileSignup() {
   const prom1 = uploadPhoto();
   const prom2 = createUser();
 
-  return Promise.all([prom1, prom2])
-    .then((value) => {
+  return Promise.all([prom1, prom2]).then((value) => {
     console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-    })
-    .catch(() => { console.log('Failed to get data') });
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
