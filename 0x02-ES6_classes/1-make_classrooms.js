36f5201fa3b0b26a8f5889de88ac3b126fa914d1
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-26 22:52:37 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return Array.of(ClassRoom(19), ClassRoom(20), ClassRoom(34));
+  return ( [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)] );
 
 }
