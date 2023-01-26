a59f7e2e8c95213d7ee7931e6c4977b46d5d1ce1
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-26 04:00:36 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const prom1 = uploadPhoto();
   const prom2 = createUser();
 
-  return Promise.all([p1, p2])
+  return Promise.all([prom1, prom2])
     .then((value) => {
     console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
