77307941cbc25f03d893a5a94cd225a3d0c9cf50
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-26 05:00:05 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,32 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
+}
+
+
+
+
+
+
+
+/*import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const f1 = signUpUser(firstName, lastName);
   const f2 = uploadPhoto(fileName);
   const proms = [f1, f2]; 
@@ -12,3 +38,4 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return final;
     
 }
+*/
