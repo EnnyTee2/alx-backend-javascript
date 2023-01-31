5f219d70894b402dbdc6fd8669ea94e1c1108ee6
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-31 22:59:18 +0100

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,5 @@
-const getStudentsByLocation = (students, city) => {
-  const arratyObjec = students.filter((funtion) => funtion.location === city);
-  return arratyObjec;
+const getStudentsByLocation = (studentlist, city) => {
+  return studentist.filter((student) => student.location === city);
 };
 
 export default getStudentsByLocation;
