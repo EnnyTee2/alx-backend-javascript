39ca6203434c6bdbcaacbf51734b205a4bcc0dec
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-01-31 22:14:12 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,12 @@
 export default const getListStudents() {
-  return Array.of({'id': 1,
-                  'firstName': 'Guillaume',
-                  'location': 'San Francisco' },
-                  {'id': 2,
-                  'firstName': 'James',
-                  'location': 'Columbia' },
-                  {'id': 3,
-                  'firstName': 'Serena',
-                  'location': 'San Francisco' }
+  return Array.of({ id: 1,
+                  firstName: 'Guillaume',
+                  location: 'San Francisco' },
+                  { id: 2,
+                  firstName: 'James',
+                  location: 'Columbia' },
+                  { id: 3,
+                  firstName: 'Serena',
+                  location: 'San Francisco' }
                   )
 }
