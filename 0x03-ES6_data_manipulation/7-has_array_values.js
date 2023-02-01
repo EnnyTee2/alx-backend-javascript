ffa960a10e7baf30f03380dd954d0894a2504974
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-01 01:27:24 +0100

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,7 @@
 export default function hasValuesFromArray(set, array) {
   const set2 = new Set(array);
   for (element of set2.values()) {
-    if !(set.has(element)) {
+    if (!set.has(element)) {
       return false;
     }
   } 
